7a5b290284c758e6098674a2a87c5b492ab2c87f
edloidas <edloidas@gmail.com>
2016-08-11 10:54:30 +0300

    Update webpack logging.
    Added error logging for the webpack.

--- modules/admin/admin-ui/gulp/util/handleErrors.js
+++ modules/admin/admin-ui/gulp/util/handleErrors.js
@@ -1,6 +1,8 @@
 var notify = require("gulp-notify");
+var gulpUtil = require("gulp-util");
+var path = require("path");
 
-module.exports = function (errorObject) {
+module.exports.handleErrors = function (errorObject) {
     var context = this || global;
     notify.onError(errorObject.toString().replace(/:\s{1}/g, ':\n')).apply(context, arguments);
 
@@ -8,4 +10,13 @@ module.exports = function (errorObject) {
     if (context && typeof context.emit === 'function') {
         global.emit('end');
     }
+};
+
+module.exports.handleWebpackErrors = function (errorObject) {
+    var logger = gulpUtil.colors.red;
+    var filePath = path.relative(__dirname + '/../..', errorObject.module.userRequest);
+    var fileName = path.basename(errorObject.module.userRequest);
+
+    var message = filePath + '\n' + fileName + ' ' + errorObject.message + '\n';
+    gulpUtil.log(logger(message));
 };
